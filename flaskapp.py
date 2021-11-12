4107d7053f054756e8fa8e4e3449e49cc1f3e7fc
Emerson Cardoso <hemersonycardoso@gmail.com>
2021-11-12 23:43:09 +0100

    Update flaskapp.py

--- flaskapp.py
+++ flaskapp.py
@@ -1,17 +1,17 @@
 from flask import Flask, jsonify
 import numpy as np
 
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route('/')
+@application.route('/')
 def index():
     return 'Hello Emerson \n'
 
-@app.route('/random/<n>')
+@application.route('/random/<n>')
 def randomvalues(n):
     values = np.random.randint(0, 10, int(n))
     result = {'values' : values.tolist()}
     return jsonify(result)
 
 if __name__ == '__main__':
-  app.run(debug=True)
+  application.run(debug=True)
