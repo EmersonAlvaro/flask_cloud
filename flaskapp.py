1671160405f56823aa608f7be62759cb5a0c0dea
Emerson Cardoso <hemersonycardoso@gmail.com>
2021-11-12 22:25:45 +0100

    flask_app

---
+++ flaskapp.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify
+import numpy as np
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Hello Emerson \n'
+
+@app.route('/random/<n>')
+def randomvalues(n):
+    values = np.random.randint(0, 10, int(n))
+    print(values)
+    result = {'values': values.tolist()}
+
+    return jsonify(result)
+
+if __name__ == '__main__':
+  app.run(debug=True)
